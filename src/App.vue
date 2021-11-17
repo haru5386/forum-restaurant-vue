cf65fd5889f389b3a9c389da12a5c36cbc8d4bff
haru5386 <snsn550@gmail.com>
2021-11-17 21:33:14 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-    <router-view />
+    <Navbar />
+    <main role="main" class="mt-5 bg-white">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+
 export default {
-  name: "App",
+  components: {
+    Navbar,
+  },
 };
 </script>
 
